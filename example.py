cc88c7512920dbd7625fc0fa9f1ef9dc5a5fc1d0
dastanNurbek <dastan.nurbek22@gmail.com>
2025-03-24 11:10:51 +0100

    Added example script

---
+++ example.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# A comment
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello, World! Hello {} ".format(recipient))
+    return recipient
